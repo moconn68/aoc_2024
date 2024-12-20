a637cb5ad3500f47905d192270e50284f5dd36be
Matthew O'Connell <mattgoconn@gmail.com>
2024-12-19 23:43:21 -0800

    Day 4 Part 2
    Solution for Day 4 Part 2

--- crates/day_04/src/main.rs
+++ crates/day_04/src/main.rs
@@ -6,9 +6,6 @@ use anyhow::Result;
 
 const INPUT_FILE: &str = "input.txt";
 
-const WORD: [char; 4] = ['X', 'M', 'A', 'S'];
-const WORD_REV: [char; 4] = [WORD[3], WORD[2], WORD[1], WORD[0]];
-
 type Matrix<T> = Vec<Vec<T>>;
 
 fn main() {
@@ -26,6 +23,9 @@ fn runner() -> Result<()> {
 
     let ans_one = part_one(&input_data);
     println!("Part one:\t{}", ans_one);
+
+    let ans_two = part_two(input_data);
+    println!("Part two:\t{}", ans_two);
 
     Ok(())
 }
@@ -45,6 +45,9 @@ where
     R: AsRef<[C]>,
     C: AsRef<[char]>,
 {
+    const WORD: [char; 4] = ['X', 'M', 'A', 'S'];
+    const WORD_REV: [char; 4] = [WORD[3], WORD[2], WORD[1], WORD[0]];
+
     let mut count = 0;
     let grid = input.as_ref();
     for (y, row) in input.as_ref().iter().map(|r| r.as_ref()).enumerate() {
@@ -87,9 +90,51 @@ where
 
                 let matches = [horiz, vert, diag_right, diag_left]
                     .into_iter()
-                    .filter(|word| word.is_some_and(|w| w == &WORD || w == &WORD_REV));
+                    .filter(|word| word.is_some_and(|w| w == WORD || w == WORD_REV));
 
                 count += matches.count();
+            }
+        }
+    }
+    count
+}
+
+fn part_two<R, C>(input: R) -> usize
+where
+    R: AsRef<[C]>,
+    C: AsRef<[char]>,
+{
+    const WORD: [char; 3] = ['M', 'A', 'S'];
+    const WORD_REV: [char; 3] = [WORD[2], WORD[1], WORD[0]];
+
+    let mut count = 0;
+    let grid = input.as_ref();
+    for (y, row) in input.as_ref().iter().map(|r| r.as_ref()).enumerate() {
+        for (x, el) in row.iter().enumerate() {
+            if WORD[0].eq(el) || WORD[WORD.len() - 1].eq(el) {
+                let diag_right = (|| -> Option<_> {
+                    let first = grid.get(y).and_then(|row| row.as_ref().get(x))?;
+                    let second = grid.get(y + 1).and_then(|row| row.as_ref().get(x + 1))?;
+                    let third = grid.get(y + 2).and_then(|row| row.as_ref().get(x + 2))?;
+                    Some([*first, *second, *third])
+                })();
+                let diag_right = diag_right.as_ref().map(|x| x.as_slice());
+
+                let diag_left = (|| -> Option<_> {
+                    let first = grid.get(y).and_then(|row| row.as_ref().get(x + 2))?;
+                    let second = grid.get(y + 1).and_then(|row| row.as_ref().get(x + 1))?;
+                    let third = grid.get(y + 2).and_then(|row| row.as_ref().get(x))?;
+                    Some([*first, *second, *third])
+                })();
+                let diag_left = diag_left.as_ref().map(|x| x.as_slice());
+
+                let arms = [diag_right, diag_left]
+                    .into_iter()
+                    .filter(|word| word.is_some_and(|w| w == WORD || w == WORD_REV));
+
+                if arms.count() == 2 {
+                    count += 1;
+                }
             }
         }
     }
@@ -117,6 +162,16 @@ MXMXAXMASX";
 
         let data = parse_input(INPUT.as_bytes()).unwrap();
         let actual = part_one(data);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let expected = 9;
+
+        let data = parse_input(INPUT.as_bytes()).unwrap();
+        let actual = part_two(data);
 
         assert_eq!(expected, actual);
     }
